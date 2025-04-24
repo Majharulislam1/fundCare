5b3d414fd9f307dbc414f829378f06c6d09e0609
Majharulislam <majhurulislam535@gmail.com>
2025-04-25 01:59:25 +0600

    complete funded_charity section and solve responsive issue

--- src/Components/Funded_Charity.jsx
+++ src/Components/Funded_Charity.jsx
@@ -1,66 +1,76 @@
 import React from 'react';
 import { FaBottleWater } from "react-icons/fa6";
+import { FaBowlFood } from "react-icons/fa6";
+import { FaHeartbeat } from "react-icons/fa";
+import { FaBook } from "react-icons/fa6";
 
 const Funded_Charity = () => {
     return (
         <div className='w-4/5 mx-auto pb-12'>
             <div className="text-center pb-16">
                 <h1 className="lg:text-5xl md:text-4xl sm:text-3xl font-bold text-secondary mb-4">We’ve funded <span className='text-primary'>120,00 charity projects</span> for <br />
-                20M people around the world.</h1>
+                    20M people around the world.</h1>
             </div>
 
 
 
-          <div className='grid grid-cols-4 gap-4'>
+            <div className='grid grid-cols-4 gap-4'>
                 <div>
                     <div>
-               
-                    <FaBottleWater  className='text-5xl text-primary'/>
+
+                        <FaBottleWater className='text-5xl text-primary' />
                     </div>
                     <div>
-                         <h4 className='text-secondary font-bold py-6 text-lg'>Pure Water <br />
-                         For Poor People</h4>
-                         <p className='text-paragraph'>663 million people drink dirty water. Learn how access to clean water can improve health, boost local economies.</p>
+                        <h4 className='text-secondary font-bold py-6 text-lg'>Pure Water <br />
+                            For Poor People</h4>
+                        <p className='text-paragraph'>663 million people drink dirty water. Learn how access to clean water can improve health, boost local economies.</p>
                     </div>
 
                 </div>
                 <div>
                     <div>
-               
-                    <FaBottleWater  className='text-5xl text-primary'/>
+
+
+
+                        <FaBowlFood className='text-5xl text-primary' />
                     </div>
                     <div>
-                         <h4 className='text-secondary font-bold py-6 text-lg'>Pure Water <br />
-                         For Poor People</h4>
-                         <p className='text-paragraph'>663 million people drink dirty water. Learn how access to clean water can improve health, boost local economies.</p>
+                        <h4 className='text-secondary font-bold py-6 text-lg'>Healthy Food <br />
+                            For Poor People</h4>
+                        <p className='text-paragraph'>663 million people drink dirty water. Learn how access to clean water can improve health, boost local economies.</p>
                     </div>
 
                 </div>
                 <div>
                     <div>
-               
-                    <FaBottleWater  className='text-5xl text-primary'/>
+
+                    
+                    <FaHeartbeat className='text-5xl text-primary' />
+
+                         
                     </div>
                     <div>
-                         <h4 className='text-secondary font-bold py-6 text-lg'>Pure Water <br />
-                         For Poor People</h4>
-                         <p className='text-paragraph'>663 million people drink dirty water. Learn how access to clean water can improve health, boost local economies.</p>
+                        <h4 className='text-secondary font-bold py-6 text-lg'>Medical <br />
+                        Facilities for People</h4>
+                        <p className='text-paragraph'>663 million people drink dirty water. Learn how access to clean water can improve health, boost local economies.</p>
                     </div>
 
                 </div>
                 <div>
                     <div>
-               
-                    <FaBottleWater  className='text-5xl text-primary'/>
+
+                      
+                        
+<FaBook className='text-5xl text-primary' />
                     </div>
                     <div>
-                         <h4 className='text-secondary font-bold py-6 text-lg'>Pure Water <br />
-                         For Poor People</h4>
-                         <p className='text-paragraph'>663 million people drink dirty water. Learn how access to clean water can improve health, boost local economies.</p>
+                        <h4 className='text-secondary font-bold py-6 text-lg'>Pure Education <br />
+                        For Every Children</h4>
+                        <p className='text-paragraph'>663 million people drink dirty water. Learn how access to clean water can improve health, boost local economies.</p>
                     </div>
 
                 </div>
-          </div>
+            </div>
 
         </div>
     );
