a038ef4b2fb04cd469e4c3f77642a6d3329883d3
Majharulislam <majhurulislam535@gmail.com>
2025-04-25 01:42:18 +0600

    Funded charity add in the home  page

---
+++ src/Components/Funded_Charity.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Funded_Charity = () => {
+    return (
+        <div className='w-4/5 mx-auto'>
+              <div className="text-center">
+          <h1 className="lg:text-5xl md:text-4xl sm:text-3xl font-bold text-secondary mb-4">Volunteer Needs</h1>
+          <p className="lg:w-1/2 md:w-3/4 sm:w-full mx-auto text-paragraph"> Join us in providing essential healthcare services to underserved communities. Help with medical camps,  </p>
+        </div>
+        </div>
+    );
+};
+
+export default Funded_Charity;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -8,6 +8,7 @@ import Team from "./Team";
 import Faq_section from "./Faq_section";
 import Our_Mission_home from "./Our_Mission_home";
 import CharitySection from "./CharitySection";
+import Funded_Charity from "./Funded_Charity";
 
 
 const Home = () => {
@@ -26,6 +27,9 @@ const Home = () => {
             <CharitySection></CharitySection>
 
             <Blogs></Blogs>
+
+ <Funded_Charity></Funded_Charity>
+
             <Home_volunteer></Home_volunteer>
             <Team></Team>
             <Faq_section></Faq_section>
